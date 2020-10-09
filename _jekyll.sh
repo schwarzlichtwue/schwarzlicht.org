79a4f2246a8a8e7bd860074da50526337782b134
schwarzlicht-wue <schwarzlicht.wuerzburg@gmail.com>
2020-10-09 10:27:37 +0200

    fixed ruanda error

--- _jekyll.sh
+++ _jekyll.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Deprecated warnings popping out for ruby v2.7.
 # This egrep removes them
-bundle exec jekyll serve -d /tmp/sl 2>&1 | egrep -v 'deprecated'
+bundle exec jekyll serve --incremental -d /tmp/sl 2>&1 | egrep -v 'deprecated'
