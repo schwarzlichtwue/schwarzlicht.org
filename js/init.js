1b9a789baf761a72fc98810e4299e05d6e750e6e
schwarzlicht-wue <schwarzlicht.wuerzburg@gmail.com>
2020-10-16 00:05:34 +0200

    fixed sidenav

--- js/init.js
+++ js/init.js
@@ -2,10 +2,12 @@ var sidenavinstances;
 var boxedinstances;
 var collapseinstances;
 var dropdowninstances;
+var autocompleteinstances;
+var tabinstances;
 
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.sidenav');
-	sidenavinstances = M.Sidenav.init(elems, {});
+	sidenavinstances = M.Sidenav.init(elems, {edge: 'left'});
 	var boxed = document.querySelectorAll('.materialboxed');
 	boxedinstances = M.Materialbox.init(boxed, {});
 	var collapse = document.querySelectorAll('.collapsible');
@@ -14,9 +16,45 @@ document.addEventListener('DOMContentLoaded', function() {
 	dropdowninstances = M.Dropdown.init(dropdown, {hover: false, alignment: 'left'});
 
 	var tabinstances = document.querySelectorAll('.tabs');
-	var instance = M.Tabs.init(tabinstances, {swipeable: true});
+	tabinstances = M.Tabs.init(tabinstances, {swipeable: true});
 
+
+/*    var scrollspies = document.querySelectorAll('.scrollspy');
+    scrollspyinstances = M.ScrollSpy.init(scrollspies, {});*/
+
+    init_autocomplete();
 
 	let materializeLoadedEvent = new CustomEvent("materializeLoaded");
 	document.dispatchEvent(materializeLoadedEvent);
  });
+
+let init_autocomplete = function () {
+    mapping = {
+		"Kontakt": "/contact/",
+		"Gruppen": "/other/groups/",
+		"Demosphere": "/other/demosphere/",
+		"Dokumente": "/other/documents/",
+		"Kalender": "/other/calendar/",
+		"Linksunten": "/other/linksunten/",
+		"Archiv": "/archive/",
+    };
+
+	let null_mapping = function () {
+        results = {};
+        for (let value in mapping) {
+            results[value] = null;
+        }
+        return results;
+    }
+
+
+    let visit_search = function (selection) {
+        window.open(mapping[selection], "_self");
+    };
+
+	var autocompletes = document.querySelectorAll('.autocomplete');
+	autocompleteinstances = M.Autocomplete.init(autocompletes, {
+		onAutocomplete: visit_search,
+		data: null_mapping()
+	});
+}
